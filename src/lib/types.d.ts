142615d66ecb1d2ed0d9c554850a228fd52fbf0b
Tiffany D <tiffanyjdow@gmail.com>
2024-09-08 11:42:57 -0300

    Fix deployment errors

---
+++ src/lib/types.d.ts
@@ -0,0 +1,24 @@
+type CollectionType = {
+  createdAt: string;
+  description: string;
+  image: string;
+  slug: string;
+  title: string;
+  updatedAt: string;
+  products: Product[];
+};
+
+type ProductType = {
+  id: string;
+  title: string;
+  description: string;
+  updatedAt: string;
+  tags: string[];
+  sizes: [];
+  price: number,
+  media: string[];
+  createdAt: string;
+  colors: string[];
+  collections: string[];
+  category: string;
+};
